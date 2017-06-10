ca8e84944b192407e478ab9bc8695ebf76f8ddae
AltangerelDondovdorj <adondovdorj@mum.edu>
2017-06-09 19:47:44 -0500

    added event listener

--- src/main/java/bean/AirlineData.java
+++ src/main/java/bean/AirlineData.java
@@ -1,9 +1,13 @@
 package bean;
 
+import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
@@ -13,16 +17,72 @@ import model.Flight;
 
 @ManagedBean(name="data", eager=true)
 @SessionScoped
-public class AirlineData {
+public class AirlineData implements Serializable{
 	
-	public List<Flight> flights;
+	private List<Flight> flights;
+	private String searchAirline = "";
+	private String searchDeparture = "";
+	private String searchDestination = "";
 	
-	public List<Flight> callFlights(){
+	public void findAirlines(AjaxBehaviorEvent event) {
 		Client client = ClientBuilder.newClient();
-		flights = client.target("http://localhost:8080/airlinesWebApp/rs/flight/all")
+		System.out.println("hello event");
+		if (searchAirline.isEmpty()) {
+			setFlights(client.target("http://localhost:8080/airlinesWebApp/rs/flight/all")
 					.request(MediaType.APPLICATION_JSON)
-					.get(new GenericType<List<Flight>>() {});
+					.get(new GenericType<List<Flight>>() {}));
+		} else {
+			
+			setFlights(client.target("http://localhost:8080/airlinesWebApp/rs/flight/findByAirline?name" + searchAirline)
+					.request(MediaType.APPLICATION_JSON)
+					.get(new GenericType<List<Flight>>() {}));
+			System.out.println("hello airlines " + flights.size());
+		}
+	}
+
+	public List<Flight> getAllFlights(){
+		Client client = ClientBuilder.newClient();
+		setFlights(client.target("http://localhost:8080/airlinesWebApp/rs/flight/all")
+					.request(MediaType.APPLICATION_JSON)
+					.get(new GenericType<List<Flight>>() {}));
 		return flights;
 	}
+	
+	public List<Flight> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(List<Flight> flights) {
+		this.flights = flights;
+	}
+	
+	public String getSearchAirline() {
+		return searchAirline;
+	}
+
+	public void setSearchAirline(String searchAirline) {
+		this.searchAirline = searchAirline;
+	}
+
+	public String getSearchDeparture() {
+		return searchDeparture;
+	}
+
+	public void setSearchDeparture(String searchDeparture) {
+		this.searchDeparture = searchDeparture;
+	}
+
+	public String getSearchDestination() {
+		return searchDestination;
+	}
+
+	public void setSearchDestination(String searchDestination) {
+		this.searchDestination = searchDestination;
+	}
+	
+/*	public List<Flight> callFlights(){
+		;
+		return flights;
+	}*/
 	
 }

b68b9395c8b6e2d4652a6ceefb772395fc2486af
AltangerelDondovdorj <adondovdorj@mum.edu>
2017-06-11 17:47:16 -0500

    Done

--- src/main/java/bean/AirlineBean.java
+++ src/main/java/bean/AirlineBean.java
@@ -1,9 +1,12 @@
 package bean;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
@@ -16,8 +19,36 @@ import model.Flight;
 @SessionScoped
 public class AirlineBean {
 	
+	
 	String airlineName;
+	
+	String oldName;
+	String newName;
 
+	
+	public String updateAirline(){
+		
+		Client client = ClientBuilder.newClient();
+		 client.target("http://localhost:8080/airlinesWebApp/rs/airline/update?old=" + oldName + "&new="+newName)
+		.request(MediaType.APPLICATION_JSON)
+		.get();	
+		
+		return "updated";
+	}
+	public String deleteAirline(){
+		
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+		        .getRequest();
+		   String id = request.getParameter("id");
+		
+		
+		Client client = ClientBuilder.newClient();
+		 client.target("http://localhost:8080/airlinesWebApp/rs/airline/delete?id=" + id)
+		.request(MediaType.APPLICATION_JSON)
+		.get();	
+		 return "deleted";
+	}
+	
 	public List<Airline> getAirlineList(){
 		Client client = ClientBuilder.newClient();
 		return client.target("http://localhost:8080/airlinesWebApp/rs/airline/airlines")
@@ -25,8 +56,20 @@ public class AirlineBean {
 		.get(new GenericType<List<Airline>>() {});	
 	}
 	
-	public void submit(){
-		System.out.println("Hello " + airlineName);
+	public String submit(){
+		Client client = ClientBuilder.newClient();
+		try{
+		 client.target("http://localhost:8080/airlinesWebApp/rs/airline/create?name=" + airlineName)
+		.request(MediaType.APPLICATION_JSON)
+		.get();		
+		}
+		catch(Exception ex){
+			//throw new Exception();
+			System.out.println("error");
+			return "error";
+		}
+		System.out.println("success");
+		return "success";
 	}
 	
 	public String getAirlineName() {
@@ -36,4 +79,22 @@ public class AirlineBean {
 	public void setAirlineName(String airlineName) {
 		this.airlineName = airlineName;
 	}
+
+	public String getOldName() {
+		return oldName;
+	}
+
+	public void setOldName(String oldName) {
+		this.oldName = oldName;
+	}
+
+	public String getNewName() {
+		return newName;
+	}
+
+	public void setNewName(String newName) {
+		this.newName = newName;
+	}
+	
+	
 }
--- src/main/java/bean/AirlineData.java
+++ src/main/java/bean/AirlineData.java
@@ -26,8 +26,7 @@ public class AirlineData implements Serializable{
 	private static final long serialVersionUID = -8161703447706643310L;
 	private List<Flight> flights;
 	private String searchAirline = "";
-	private String searchDeparture = "";
-	private String searchDestination = "";
+	private String fromDate = "";
 	
 	public void findAirlines(AjaxBehaviorEvent event) {
 		Client client = ClientBuilder.newClient();
@@ -41,6 +40,21 @@ public class AirlineData implements Serializable{
 					.get(new GenericType<List<Flight>>() {}));
 		}
 	}
+	
+	public void findFlightbyDate(){
+		Client client = ClientBuilder.newClient();
+		System.out.println(fromDate);
+		if (fromDate.isEmpty()) {
+			setFlights(client.target("http://localhost:8080/airlinesWebApp/rs/flight/all")
+					.request(MediaType.APPLICATION_JSON)
+					.get(new GenericType<List<Flight>>() {}));
+		} else {
+			setFlights(client.target("http://localhost:8080/airlinesWebApp/rs/flight/findByArrival?datetime=" + fromDate)
+					.request(MediaType.APPLICATION_JSON)
+					.get(new GenericType<List<Flight>>() {}));
+		}
+	}
+	
 	@PostConstruct
 	public void getAllFlights(){
 		Client client = ClientBuilder.newClient();
@@ -65,26 +79,13 @@ public class AirlineData implements Serializable{
 	public void setSearchAirline(String searchAirline) {
 		this.searchAirline = searchAirline;
 	}
-
-	public String getSearchDeparture() {
-		return searchDeparture;
+	public String getFromDate() {
+		return fromDate;
+	}
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
 	}
 
-	public void setSearchDeparture(String searchDeparture) {
-		this.searchDeparture = searchDeparture;
-	}
-
-	public String getSearchDestination() {
-		return searchDestination;
-	}
-
-	public void setSearchDestination(String searchDestination) {
-		this.searchDestination = searchDestination;
-	}
 	
-/*	public List<Flight> callFlights(){
-		;
-		return flights;
-	}*/
 	
 }
